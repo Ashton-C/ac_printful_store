5260bc70386a8a3eefca8d673de113978e289d3b
Ashton Christensen <armadillomaster@gmail.com>
2023-01-05 11:49:23 -0800

    LOCAL FONT ADDED BOOYAH

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  module: {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    loader: "url-loader",
+  },
   images: {
     domains: ["files.cdn.printful.com"],
   },
