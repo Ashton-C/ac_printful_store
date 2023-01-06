e67f9b37aeb7119271b4c4bcc66e264865349627
Ashton Christensen <armadillomaster@gmail.com>
2023-01-05 21:05:52 -0800

    idk whats happening...

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   module: {
+<<<<<<< HEAD
+    test: /\.(png|gif|woff|woff2|eot|ttf|svg)$/,
+    loader: "url-loader?limit=100000",
+=======
     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
     loader: "url-loader",
+>>>>>>> 5260bc70386a8a3eefca8d673de113978e289d3b
   },
   images: {
     domains: ["files.cdn.printful.com"],
